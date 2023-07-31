04a4e74e4ef8d4356ef462b18645ecddd845b6e8
Mustafa Sharif <Mustafa.Sharif@datadrivenlogistics.com>
2023-07-31 16:06:47 +0100

    Initial commit

---
+++ app/src/main/java/com/example/coroutinerefreshment/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.coroutinerefreshment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import kotlinx.coroutines.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var myTextView: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        myTextView = findViewById<TextView?>(R.id.textId)
+
+        Log.d("MainActivity", "i am in Main Thread: ")
+        GlobalScope.launch(Dispatchers.Default){
+            printMyTextAfterDelay("Love it")
+        }
+        Log.d("MainActivity", "i am Back to the Main Thread: ")
+    }
+
+    suspend fun printMyTextAfterDelay(myText: String){
+        GlobalScope.launch(Dispatchers.IO) {
+            delay(2000)
+            Log.d("MainActivity", "i am in IO Thread: ")
+        }
+    }
+}
